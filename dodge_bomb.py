14e2c6b8a4cf4c137162031c45595e4a4aa689b9
c0a241185d <c0a241185d@edu.teu.ac.jp>
2025-04-22 14:59:37 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,14 +9,28 @@ DELTA = {
             pg.K_DOWN: (0,5),
             pg.K_LEFT: (-5,0),
             pg.K_RIGHT: (5,0),
-        }  #DELTAの定義
+        }  # DELTAの定義
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果、縦方向判定結果）
+    画面内ならTrue,画面外ならFalse
+    """
+    yoko, tate = True, True  # 横、縦方向用の変数
+    # 横方向判定
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    #こうかとん初期化
+    # こうかとん初期化
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
@@ -31,11 +45,16 @@ def main():
     vy = 5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+
+        # if kk_rct.colliderect(bb_rct):
+        #     print("Game Over")
+        #     return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -46,8 +65,16 @@ def main():
                 sum_mv[1] += mv[1]  #上下方向
 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 画面の外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
+
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: # 左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate: # 上下どちらかにはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
